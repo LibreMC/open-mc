34ebad427ac372041a92480ea3a6ca312dc0fb50
Hannah <salvadin2@gmail.com>
2022-11-21 17:04:35 -0500

    pls work

--- HttpResponse.php
+++ HttpResponse.php
@@ -4,9 +4,9 @@ class HttpResponse
     public $text;
     public $code;
     
-    public function __construct(string $text, int $responseCode)
+    public function __construct(string $text, int $code)
     {
         $this->text = $text;
-        $this->code = $responseCode;
+        $this->code = $code;
     }
 }
